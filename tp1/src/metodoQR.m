8616a12e0c2aa73eaad239cc7e79b9ed450d51fb
Juan Pablo Civile <elementohb@gmail.com>
2012-09-15 18:09:51 -0300

    Clean up QR method a bit. Normalize output Q.

--- tp1/src/metodoQR.m
+++ tp1/src/metodoQR.m
@@ -1,15 +1,28 @@
-function [T, Q] = metodoQR(A)
+function [Q, T] = metodoQR(A)
 
-	T = A;
-	n = 0;
-	previous = zeros(size(A));
-	Q = eye(size(A));
-	while norm(previous-T) > 10**-5
-		previous = T;
-		[Qk , R] = qr(T);
-		T = R*Qk;
-		n += 1;
-		Q = Q*Qk;
-	end
-	disp("Number of iterations:"), disp(n);
+    [n, m] = size(A);
+    %T = hess(A);
+    T = A;
+
+    iterations = 0;
+    previous = zeros(size(A));
+    Q = eye(size(A));
+
+    while norm(previous - T) > 10**-6
+
+        previous = T;
+
+        [Qk , R] = qr(T);
+
+        T = R*Qk;
+        Q = Q*Qk;
+
+        iterations += 1;
+    end
+
+    for i = 1:n
+        Q(:,i) = Q(:,i) / norm(Q(:,i));
+    end
+
+    disp("Number of iterations:"), disp(iterations);
 end
