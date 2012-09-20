2a9557e8613fdee7eb4d9e0e86969707379fa6e5
Juan Pablo Civile <elementohb@gmail.com>
2012-09-19 22:06:58 -0300

    Make sure we can store the image

--- tp1/src/compresionBruta1.m
+++ tp1/src/compresionBruta1.m
@@ -1,4 +1,4 @@
-function [res] = compresionBruta1(path)
+function [res] = compresionBruta1(path, out)
 
 a = imread(path);
 imshow(a) % muestro la imagen original
@@ -8,6 +8,7 @@ m = m.';
 M = repmat(m,1,1024);
 d = col2im(M,[16,16],[512,512], "distinct");
 meanimg = uint8(d);
-imshow(meanimg) % muestro la imagen "media"
+imshow(meanimg); % muestro la imagen "media"
+imwrite(meanimg, out);
 
 end
