8cc7d74c4f46e1cd0dffa43051cc5a2fa11b4277
Juan Pablo Civile <elementohb@gmail.com>
2012-11-22 22:26:26 -0300

    Only one call to colormap should suffice

--- tp2/tp2.m
+++ tp2/tp2.m
@@ -11,14 +11,12 @@ function tp2()
     %print amplitude image
     %amp = normalization(abs(X), 0, 255);
     amp = abs(X);
-    colormap(gray(255));
     image(amp');
     title('Imagen de la amplitud');
     print('-dpng', './images/amplitude.png');
 
     %print phase image
     phase = normalization(angle(X), 0, 255);
-    colormap(gray(255));
     image(phase');
     title('Imagen de la fase');
     print('-dpng', './images/phase.png');
@@ -26,26 +24,22 @@ function tp2()
     %rebuild original image
     %rebuild = abs(ifft2(X));
     rebuild = real(ifft2(X));
-    colormap(gray(255));
     image(rebuild');
     title('Imagen reconstruida con la Transformada Inversa Discreta');
     print('-dpng', './images/rebuild.png');
 
     %apply custom filter
     customFilteredImg = customFilter(X);
-    colormap(gray(255));
     image(customFilteredImg');
     title('Imagen tras aplicación del filtro custom');
 
     %apply gauss filter
     gaussFilteredImg = gaussFilter(X);
-    colormap(gray(255));
     image(gaussFilteredImg');
     title('Imagen tras aplicación del filtro gaussiano');
 
     %apply damero filter
     dameroFilteredImg = dameroFilter(X);
-    colormap(gray(255));
     image(dameroFilteredImg');
     title('Imagen tras aplicación del filtro damero');
 
