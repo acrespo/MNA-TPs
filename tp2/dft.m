e8e02351567ddbec9f470c0f4e6536a65b50cac1
Juan Pablo Civile <elementohb@gmail.com>
2012-11-23 01:24:39 -0300

    Fix the discrete transform implementation (off by one errors)

--- tp2/dft.m
+++ tp2/dft.m
@@ -3,21 +3,21 @@ function res = dft(x)
 %   x must be square
     res = zeros(size(x));
     N = rows(x);
-    for i = 1:(N - 1)
-        for j = 1:(N - 1)
+    for i = 1:N
+        for j = 1:N
             res(i, j) = _dft(x, i, j);
         end
     end
 end
 
-function res = _dft(x, i, j)
+function res = _dft(x, l, k)
 
 %   X must be square
     res = 0;
     N = rows(x);
     for n = 1:N
         for m = 1:N
-            res += x(n, m) * e^(-i*2*pi*(n*i + m*j)/N);
+            res += x(n, m) * e^(-i*2*pi*((n - 1)*(l - 1) + (m - 1)*(k - 1))/N);
         end
     end
 end
