377c30f08bf3035e74f3ec31e600a3e9e5561acb
Juan Pablo Civile <elementohb@gmail.com>
2012-11-22 22:21:30 -0300

    Move auxiliary functions into the files that use them

--- tp2/dft.m
+++ tp2/dft.m
@@ -1,4 +1,4 @@
-function res = dft(x) 
+function res = dft(x)
 
 %   x must be square
     res = zeros(size(x));
@@ -6,6 +6,18 @@ function res = dft(x)
     for i = 1:(N - 1)
         for j = 1:(N - 1)
             res(i, j) = _dft(x, i, j);
+        end
+    end
+end
+
+function res = _dft(x, i, j)
+
+%   X must be square
+    res = 0;
+    N = rows(x);
+    for n = 1:N
+        for m = 1:N
+            res += x(n, m) * e^(-i*2*pi*(n*i + m*j)/N);
         end
     end
 end
