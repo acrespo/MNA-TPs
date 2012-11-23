3377c491dfb683d6022d373fce6e98cdc204c57b
acrespo <alvaro.andres.crespo@gmail.com>
2012-11-23 03:45:21 -0300

    Fixed tiny bug in gaussFilter.

--- tp2/gaussFilter.m
+++ tp2/gaussFilter.m
@@ -5,7 +5,7 @@ function res = gaussFilter(X)
     N = rows(X);
     for i = 1:N
         for j = 1:N
-            filter(i, j) = e^(-0,1 * (i^2 + 1^2));
+            filter(i, j) = e^(-0.1 * (i^2 + 1^2));
         end
     end
     res = filter .* X;
